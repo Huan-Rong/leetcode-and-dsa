1090821d10cbc6914ddd39cc9baa6c78737745e5
Huan-Rong <huanrong.maybe@gmail.com>
2021-01-26 12:06:12 +0800

    create(p88): commit solution for p88

---
+++ src/main/java/site/bulibucai/P88.java
@@ -0,0 +1,73 @@
+package site.bulibucai;
+
+import java.util.Arrays;
+
+/**
+ * 题目链接：https://leetcode-cn.com/problems/merge-sorted-array/
+ * <p>
+ * 解题要点：
+ * 1.没有要求原地操作，因此可以使用提高空间复杂度。
+ * 2.m 和 n 表示数组的元素个数，这其实也是一种逻辑分割数组，即数组的左边是有元素的，数组的右边是没有元素的。
+ * 3.数组是可以双向遍历的。
+ * 4.题目明确说明了 nums1.length == m + n
+ */
+public class P88 {
+    /**
+     * 分析：
+     * 1.将 nums2 直接合并在 nums1 的尾部，再进行排序。
+     * 2.时间复杂度：O((n+m)log(n+m))，具体需要考虑排序的性能。
+     * 3.空间复杂度：O(1)。
+     */
+    public static int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
+        return nums1;
+    }
+
+    /**
+     * 分析：
+     * 1.时间复杂度：O(m+n)。题目没有要求原地操作，因此可以使用复杂度高一些的方法。
+     * 2.空间复杂度：O(m)。
+     * 3.这个解法提高了空间复杂度，显然是没有充分利用 nums1 数组的长度是足够的这一特点。
+     */
+    public static int[] merge2(int[] nums1, int m, int[] nums2, int n) {
+        int[] tmp = new int[m];
+        System.arraycopy(nums1, 0, tmp, 0, m);
+
+        int i = 0;//index for tmp
+        int j = 0;//index for nums2
+        int k = 0;//index for nums1
+        //这段"比较赋值"的核心代码需要非常熟练。
+        while (i < m && j < n) {
+            nums1[k++] = tmp[i] < nums2[j] ? tmp[i++] : nums2[j++];
+        }
+        if (i < m) {
+            System.arraycopy(tmp, i, nums1, k, m + n - i - j);
+        }
+        if (j < n) {
+            System.arraycopy(nums2, j, nums1, k, m + n - i - j);
+        }
+        return nums1;
+    }
+
+    /**
+     * 分析：
+     * 1.时间复杂度：O(m+n)。
+     * 2.空间复杂度：O(1)。
+     */
+    public static int[] merge3(int[] nums1, int m, int[] nums2, int n) {
+        int k = m + n - 1;
+        int i = m - 1;
+        int j = n - 1;
+
+        while (i >= 0 && j >= 0) {
+            nums1[k--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+        }
+        if (j >= 0) {
+            System.arraycopy(nums2, 0, nums1, 0, j + 1);
+        }
+        // i > 0 的情况不需要做多余的复制操作。
+
+        return nums1;
+    }
+}
---
+++ src/test/java/site/bulibucai/P88Test.java
@@ -0,0 +1,58 @@
+package site.bulibucai;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class P88Test {
+
+    static Stream<Arguments> getArguments() {
+        return Stream.of(
+                Arguments.arguments(
+                        new int[]{1, 2, 3, 4, 7},
+                        new int[]{4, 7, 0, 0, 0},
+                        2,
+                        new int[]{1, 2, 3},
+                        3
+                ),
+//              下面这组测试数据不可用。因为题目明确说明了 nums1.length = m + n。
+//              另外，这组测试数据对于第一种"先合并后排序"的解法不适用。
+//                Arguments.arguments(
+//                        new int[]{1, 2, 3, 4, 7, 0, 0, 0, 0, 0},
+//                        new int[]{4, 7, 0, 0, 0, 0, 0, 0, 0, 0},
+//                        2,
+//                        new int[]{1, 2, 3},
+//                        3
+//                ),
+                Arguments.arguments(
+                        new int[]{1, 2, 3, 3, 3, 4, 6},
+                        new int[]{1, 2, 3, 4, 0, 0, 0},
+                        4,
+                        new int[]{3, 3, 6},
+                        3
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getArguments")
+    void merge(int[] expected, int[] nums1, int m, int[] nums2, int n) {
+        assertArrayEquals(expected, P88.merge(nums1, m, nums2, n));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getArguments")
+    void merge2(int[] expected, int[] nums1, int m, int[] nums2, int n) {
+        assertArrayEquals(expected, P88.merge2(nums1, m, nums2, n));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getArguments")
+    void merge3(int[] expected, int[] nums1, int m, int[] nums2, int n) {
+        assertArrayEquals(expected, P88.merge3(nums1, m, nums2, n));
+    }
+}
